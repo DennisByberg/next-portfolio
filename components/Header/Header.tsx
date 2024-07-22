aeb6c08c390328dbfeeeca8c5180e8b74dccf9eb
Dennis Byberg <dennis.byberg@hotmail.com>
2024-07-22 22:49:10 +0200

    added styling + framer motion to project

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { links } from "@/lib/data";
+import { motion } from "framer-motion";
+import Link from "next/link";
 import "./Header.scss";
 
 export default function Header() {
   return (
     <header>
-      <div></div>
+      <motion.div
+        className="header__background"
+        initial={{ y: -100, opacity: 0, x: "-50%" }}
+        animate={{ y: 0, opacity: 1, x: "-50%" }}
+      >
+        <nav>
+          <ul>
+            {links.map((link) => (
+              <motion.li key={link.hash} initial={{ y: -30, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
+                <Link className="link transition" href={link.hash}>
+                  {link.name}
+                </Link>
+              </motion.li>
+            ))}
+          </ul>
+        </nav>
+      </motion.div>
     </header>
   );
 }
