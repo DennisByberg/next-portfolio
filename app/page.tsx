7a6374737da761dc0343233a68d5511bae0e2e35
Dennis Byberg <dennis.byberg@hotmail.com>
2024-08-02 14:33:26 +0200

    Refactor readme + page

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,25 @@ import Intro from '@/components/Intro/Intro';
 import Projects from '@/components/Projects';
 import SectionDivider from '@/components/SectionDivider';
 import Skills from '@/components/Skills';
-import './page.scss';
+import { Box, SxProps, Theme } from '@mui/material';
 
 export default function Home() {
   return (
-    <main>
+    <Box component={'main'} sx={HOME_MAIN_STYLE}>
       <Intro />
       <SectionDivider />
       <About />
       <Projects />
       <Skills />
       <Contact />
-    </main>
+    </Box>
   );
 }
+
+// Material UI Home styles
+const HOME_MAIN_STYLE: SxProps<Theme> = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  paddingInline: '1rem',
+};
