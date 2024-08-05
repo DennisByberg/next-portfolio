1a9cf857b0a5dd6295052bc60b33aea10abf34fe
Dennis Byberg <dennis.byberg@hotmail.com>
2024-08-05 13:54:05 +0200

    Refactoring: Skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,14 +1,32 @@
 'use client';
-
 import SKILLS_DATA from '@/data/skillsData';
 import { useSectionInView } from '@/hooks/useSectionInView';
-import { Box, SxProps, Theme } from '@mui/material';
-import { common, grey } from '@mui/material/colors';
+import {
+  SKILLS_LIST_ITEM_STYLE,
+  SKILLS_LIST_STYLE,
+  SKILLS_SECTION_STYLE,
+} from '@/styles/skillsSX';
+import { Box } from '@mui/material';
 import { motion } from 'framer-motion';
 import SectionHeading from './SectionHeading';
 
-export default function Skills() {
+function Skills() {
   const { ref } = useSectionInView('Skills');
+
+  // Framer Motion variants
+  const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
 
   return (
     <Box component={'section'} sx={SKILLS_SECTION_STYLE} id={'skills'} ref={ref}>
@@ -17,7 +35,7 @@ export default function Skills() {
         {SKILLS_DATA.map((skill, index) => (
           <Box
             component={motion.li}
-            sx={SKILLS_LISTITEM_STYLE}
+            sx={SKILLS_LIST_ITEM_STYLE}
             key={index}
             variants={fadeInAnimationVariants}
             initial={'initial'}
@@ -33,44 +51,4 @@ export default function Skills() {
   );
 }
 
-// Framer Motion variants
-const fadeInAnimationVariants = {
-  initial: {
-    opacity: 0,
-    y: 100,
-  },
-  animate: (index: number) => ({
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: 0.05 * index,
-    },
-  }),
-};
-
-// Material UI Skills styles
-const SKILLS_SECTION_STYLE: SxProps<Theme> = {
-  marginBottom: '3rem',
-  maxWidth: '53rem',
-  scrollMarginTop: '3rem',
-  textAlign: 'center',
-
-  '@media (min-width: 640px)': {
-    marginBottom: '10rem',
-  },
-};
-
-const SKILLS_LIST_STYLE: SxProps<Theme> = {
-  display: 'flex',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
-  gap: '0.5rem',
-  color: grey[800],
-};
-
-const SKILLS_LISTITEM_STYLE: SxProps<Theme> = {
-  backgroundColor: common.white,
-  border: '1px solid rgba(0, 0, 0, 0.1)',
-  borderRadius: '13px',
-  padding: '0.75rem 1.25rem',
-};
+export default Skills;
---
+++ styles/skillsSX.ts
@@ -0,0 +1,28 @@
+import { SxProps, Theme } from '@mui/material';
+import { common, grey } from '@mui/material/colors';
+
+export const SKILLS_SECTION_STYLE: SxProps<Theme> = {
+  marginBottom: '3rem',
+  maxWidth: '53rem',
+  scrollMarginTop: '3rem',
+  textAlign: 'center',
+
+  '@media (min-width: 640px)': {
+    marginBottom: '10rem',
+  },
+};
+
+export const SKILLS_LIST_STYLE: SxProps<Theme> = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  gap: '0.5rem',
+  color: grey[800],
+};
+
+export const SKILLS_LIST_ITEM_STYLE: SxProps<Theme> = {
+  backgroundColor: common.white,
+  border: '1px solid rgba(0, 0, 0, 0.1)',
+  borderRadius: '13px',
+  padding: '0.75rem 1.25rem',
+};
