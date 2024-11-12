cafebbceb8c72baa548e56a2cd4dde630ecc0c67
94denbyb <94denbyb@gafe.molndal.se>
2024-11-12 23:43:48 +0100

    Add new global type

--- types/globalTypes.ts
+++ types/globalTypes.ts
@@ -1,3 +1,13 @@
 import LINKS_DATA from '../data/linksData';
 
 export type TSectionName = (typeof LINKS_DATA)[number]['name'];
+
+export type TExperienceItem = {
+  id: string;
+  title: string;
+  organization: string;
+  isActive: boolean;
+  isEducation: boolean;
+  startYear: number;
+  endYear: number | null;
+};
